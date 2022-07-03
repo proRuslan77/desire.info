94a4022fc62618911f337fac958fec9e56510c3e
proRuslan77 <you@example.com>
2022-07-03 22:59:53 +0300

    main page begin

--- gulpfile.js
+++ gulpfile.js
@@ -35,6 +35,7 @@ function images() {
 function scripts() {
     return src([
         'node_modules/jquery/dist/jquery.js',
+        'node_modules/slick-carousel/slick/slick.js',
         'app/js/main.js'
     ])
         .pipe(concat('main.min.js'))
